9edc698a1de1a673c58ddd86b5b83eaec0a4fccb
Samuel Orozco <68511890+StockMaster123@users.noreply.github.com>
2022-11-30 07:22:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
+const app = require('./src/app');
 const express =  require('express');
 
 const app = express();
-
-app.use(express.json());
 const PORT = process.env.PORT || 200;
-
-app.get('/', (req, res) =>{
-    res.send('Hola Mundo');
-})
 
 app.listen(PORT, ()=>{
     console.log('');
